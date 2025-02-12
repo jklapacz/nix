36abddff1b5ad32a97121a4316f6eabb0571327a
Jakub Klapacz <kubaklapacz@gmail.com>
2025-02-11 23:56:24 -0600

    Use nix-homebrew to manage homebrew installation

--- flake.nix
+++ flake.nix
@@ -2,63 +2,74 @@
   description = "Shadowfax system configuration";
 
   inputs = {
-    fh.url = "https://flakehub.com/f/DeterminateSystems/fh/*";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin = {
-      url = "github:LnL7/nix-darwin";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    mac-app-util.url = "github:hraban/mac-app-util";
+    darwin = {
+      url = "github:LnL7/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-homebrew = {
+      url = "github:zhaofengli-wip/nix-homebrew";
+    };
+    homebrew-bundle = {
+      url = "github:homebrew/homebrew-bundle";
+      flake = false;
+    };
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
+    mac-app-util = {
+      url = "github:hraban/mac-app-util";
+    };
   };
 
   outputs =
     inputs@{
       self,
-      nix-darwin,
-      nixpkgs,
+      darwin,
+      nix-homebrew,
+      homebrew-bundle,
+      homebrew-core,
+      homebrew-cask,
       home-manager,
-      fh,
+      nixpkgs,
       mac-app-util,
       ...
     }:
     let
+      user = "jklapacz";
       configuration =
         { pkgs, ... }:
         {
           nixpkgs.config.allowUnfree = true;
-          services.nix-daemon.enable = true;
-          nix.settings = {
-            experimental-features = [
-              "nix-command"
-              "flakes"
-            ];
-            max-jobs = "auto";
-            bash-prompt-prefix = "(nix:$name)\040";
-            netrc-file = "/nix/var/determinate/netrc";
-            post-build-hook = "/nix/var/determinate/post-build-hook.sh";
-            always-allow-substitutes = true;
-            extra-substituters = [ "https://cache.flakehub.com" ];
-            extra-trusted-public-keys = [
-              "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
-              "cache.flakehub.com-4:Asi8qIv291s0aYLyH6IOnr5Kf6+OF14WVjkE6t3xMio="
-              "cache.flakehub.com-5:zB96CRlL7tiPtzA9/WKyPkp3A2vqxqgdgyTVNGShPDU="
-              "cache.flakehub.com-6:W4EGFwAGgBj3he7c5fNh9NkOXw0PUVaxygCVKeuvaqU="
-              "cache.flakehub.com-7:mvxJ2DZVHn/kRxlIaxYNMuDG1OvMckZu32um1TadOR8="
-              "cache.flakehub.com-8:moO+OVS0mnTjBTcOUh2kYLQEd59ExzyoW1QgQ8XAARQ="
-              "cache.flakehub.com-9:wChaSeTI6TeCuV/Sg2513ZIM9i0qJaYsF+lZCXg0J6o="
-              "cache.flakehub.com-10:2GqeNlIp6AKp4EF2MVbE1kBOp9iBSyo0UPR9KoR0o1Y="
-            ];
-            upgrade-nix-store-path-url = "https://install.determinate.systems/nix-upgrade/stable/universal";
-            extra-nix-path = "nixpkgs=flake:https://flakehub.com/f/DeterminateSystems/nixpkgs-weekly/*.tar.gz";
-            ssl-cert-file = "/private/etc/nix/macos-keychain.crt";
-          };
 
           system.configurationRevision = self.rev or self.dirtyRev or null;
           system.stateVersion = 4;
+          system = {
+            defaults = {
+              CustomUserPreferences = {
+                "com.cursor.app" = {
+                  ApplePressAndHoldEnabled = false;
+                };
+              };
+              NSGlobalDomain = {
+                ApplePressAndHoldEnabled = false;
+
+                # 120, 90, 60, 30, 12, 6, 2
+                KeyRepeat = 2;
+                # 120, 94, 68, 35, 25, 15
+                InitialKeyRepeat = 15;
+              };
+            };
+          };
 
           # Replace with x86_64-darwin if on Intel Mac
           nixpkgs.hostPlatform = "aarch64-darwin";
@@ -74,18 +85,7 @@
             pkgs.docker
             pkgs.neofetch
             pkgs.neovim
-            fh.packages.aarch64-darwin.default
           ];
-
-          launchd.user.agents.docker-desktop = {
-            serviceConfig = {
-              ProgramArguments = [
-                "/Applications/Docker.app/Contents/MacOS/Docker Desktop.app/Contents/MacOS/Docker Desktop"
-              ];
-              KeepAlive = false;
-              RunAtLoad = true;
-            };
-          };
 
           homebrew = {
             enable = true;
@@ -96,12 +96,8 @@
             taps = [ ];
             casks = [
               "docker"
-              "font-fira-code"
-              "font-fira-mono"
-              "font-fira-mono-for-powerline"
-              "font-fira-sans"
-              "font-hack-nerd-font"
               "google-chrome"
+              "moom"
               "slack"
             ];
           };
@@ -158,10 +154,6 @@
             };
           };
 
-          programs.vscode = {
-            enable = true;
-          };
-
           home.file.".ssh/config".text = ''
             # Default GitHub account (work)
             Host github.com
@@ -205,12 +197,16 @@
         };
     in
     {
-      darwinConfigurations."Shadowfax" = nix-darwin.lib.darwinSystem {
+      darwinConfigurations."Shadowfax" = darwin.lib.darwinSystem {
         modules = [
           configuration
           mac-app-util.darwinModules.default
           home-manager.darwinModules.home-manager
+          nix-homebrew.darwinModules.nix-homebrew
           {
+            nix = {
+              enable = false;
+            };
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.verbose = true;
@@ -218,6 +214,17 @@
             home-manager.sharedModules = [
               mac-app-util.homeManagerModules.default
             ];
+            nix-homebrew = {
+              inherit user;
+              enable = true;
+              taps = {
+                "homebrew/homebrew-core" = homebrew-core;
+                "homebrew/homebrew-cask" = homebrew-cask;
+                "homebrew/homebrew-bundle" = homebrew-bundle;
+              };
+              mutableTaps = false;
+              autoMigrate = true;
+            };
           }
         ];
       };
